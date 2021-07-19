53eb75230fdc0457a6751d664a7098997c3f249d
a668640 <kshahriyar4@gmail.com>
2021-07-19 17:29:53 -0400

    updates 7/19

--- dice_game.py
+++ dice_game.py
@@ -1,15 +1,25 @@
 import random
 
+def main():
+    p1 = input("Player #1 name: ")
+    p2 = input("Player #2 name: ")
+
+    roll1 = roll_dice()
+    roll2 = roll_dice()
+
+    print(p1, "rolled", roll1)
+    print(p2, "rolled", roll2)
+
+    if roll1 > roll2:
+        print(p1, "Wins!")
+    elif roll2 > roll1:
+        print(p2, "Wins!")
+    else:
+        print("You tie!")
+
 def roll_dice():
     dice_total = random.randint(1, 6) + random.randint(1, 6)
     return dice_total
 
 
-p1 = input("Player #1 name: ")
-p2 = input("Player #2 name: ")
-
-roll1 = roll_dice()
-roll2 = roll_dice()
-
-print(p1, "rolled", roll1)
-print(p2, "rolled", roll2)
+main()
--- weather.py
+++ weather.py
@@ -1,18 +1,26 @@
 import requests
 
-api_key = "9722baba4f7af43dc75fdb06a1de1bf2"
-city = "Dallas"
-url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=imperial".format(city, api_key)
+def get_desc_and_temp():
+    api_key = "9722baba4f7af43dc75fdb06a1de1bf2"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=imperial".format("Dallas", api_key)
 
-request = requests.get(url)
-json = request.json()
-#print(json)
+    request = requests.get(url)
+    json = request.json()
+    #print(json)
 
-desc = json.get("weather")[0].get("description")
-temp_min = int(json.get("main").get("temp_min"))
-temp_max = int(json.get("main").get("temp_max"))
+    desc = json.get("weather")[0].get("description")
+    temp_min = int(json.get("main").get("temp_min"))
+    temp_max = int(json.get("main").get("temp_max"))
+    city = json.get("name")
+    return {"description": desc, "temp_min": temp_min, "temp_max": temp_max, "city": city}
 
-print("[{} Weather Information]".format(city))
-print("Today's forecast: {}".format(desc))
-print("Minimum temperature: {}".format(temp_min))
-print("Maximum temperature: {}".format(temp_max))
+def main():
+    #Results
+    weather_dict = get_desc_and_temp()
+    print("[{} Weather Information]".format(weather_dict.get("city")))
+    print("Today's forecast: {}".format(weather_dict.get('description')))
+    print("Minimum temperature: {}".format(weather_dict.get('temp_min')))
+    print("Maximum temperature: {}".format(weather_dict.get('temp_max')))
+
+
+main()
